b2f54870c12e08d315937f0c65051b3739b02e78
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2020-09-01 15:25:26 -0500

    Cards

--- src/components/heroe/HeroeList.jsx
+++ src/components/heroe/HeroeList.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { getHeroeByPublisher } from '../../selectors/getHeroesByPublisher';
+import { HeroeCard } from './HeroeCard';
 
 
 export const HeroeList =(props)=>{
     const {publisher} = props;
     const heroes_publusher = getHeroeByPublisher(publisher);
     return(
-        <ul>
+        <div className="card-column">
             {
                 heroes_publusher.map(h=>(
-                <li key={h.id}>{h.superhero}</li>
+                <HeroeCard  {...h} key={h.id}/>
                 ))
             }
-        </ul>
+        </div>
     );
 }
