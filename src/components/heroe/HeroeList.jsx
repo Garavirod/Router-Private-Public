8455cf93b9f8aa15549ad0ad56a05a23b14f22e9
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2020-09-02 13:45:57 -0500

    animation

--- src/components/heroe/HeroeList.jsx
+++ src/components/heroe/HeroeList.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { getHeroeByPublisher } from '../../selectors/getHeroesByPublisher';
 import { HeroeCard } from './HeroeCard';
 
 
 export const HeroeList =(props)=>{
     const {publisher} = props;
-    const heroes_publusher = getHeroeByPublisher(publisher);
+
+    /**
+     * when the state change this compoennet is executed again,
+     * so to avoid that we can useMemo in some blocks code for
+     * to be executed when  it be necesary and not always
+     * 
+     * for example heroes_publisher is a function that allways is
+     * executed each time the state change.
+     */
+
+    const heroes_publusher = useMemo(()=>
+        getHeroeByPublisher(publisher),
+        [publisher], //This only is activated if publisher changes
+    );
+    // const heroes_publusher = getHeroeByPublisher(publisher);
     return(
-        <div className="card-column">
+        <div className="card-column animate__animated animate__fadeIn">
             {
                 heroes_publusher.map(h=>(
                 <HeroeCard  {...h} key={h.id}/>
--- src/components/heroe/HeroeScreen.jsx
+++ src/components/heroe/HeroeScreen.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useParams, Redirect } from "react-router-dom";
 import { getHeroeById } from "../../selectors/getHeroeById";
 
@@ -7,7 +7,13 @@ export const HeroeScreen = (props) => {
   const {history} = props;
 
   const { heroeId } = useParams(); //Hook which extract the params
-  const dataHero = getHeroeById(heroeId);
+
+  // Usig useMemo for optimazing the code
+  const dataHero = useMemo( ()=>
+    getHeroeById(heroeId),
+    [heroeId], //Only is activated if heroId changes
+  );
+
   // if not exist the id
   if (!dataHero) return <Redirect to="/" />;
   // Desestructuture the data
@@ -35,10 +41,10 @@ export const HeroeScreen = (props) => {
         <img
           src={`../assets/heroes/${heroeId}.jpg`}
           alt={superhero}
-          className="img-thumbnail"
+          className="img-thumbnail animate__animated animate__fadeInLeft"
         />
       </div>
-      <div className="col-8">
+      <div className="col-8 animate__animated animate__fadeInUp">
         <h3>{superhero}</h3>
         <ul className="list-group list-group-flush">
           <li className="list-group-item">
